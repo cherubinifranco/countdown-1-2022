d2b118b49033e5a3b6a401a3ee6bed23c657f29e
Cherubini Franco <cherubini.franco@hotmail.com>
2022-02-03 17:37:50 -0300

    Fixed the whole clock
    Added a function to make the countdown work better, and fixed how it counts and displays the time.

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-const timeTotal = document.getElementById("totalTime");
-const rt = document.getElementById("remainingTime");
+const circle = document.getElementById("circle");
+const remainingTime = document.getElementById("remainingTime");
 const ct = document.getElementById("currentTime");
 const playPause = document.getElementById("playPause");
 const restartBtn = document.getElementById("restart");
+
+let timeCount;
+let minutes;
+let seconds;
 
 var running = false;
 
 // This part selects the page
 
-pag(2);
+pag(1);
 function pag(n){
     let page1 = document.getElementById("page1");
     let page2 = document.getElementById("page2");
     let page3 = document.getElementById("page3");
+
     page1.style.display = "none";
     page2.style.display = "none";
     page3.style.display = "none";
+
     if (n == 1){
         page1.style.display = "flex";
+        circle.style.backgroundColor = "#D0A85C";
     } else if (n == 2) {
         page2.style.display = "flex";
+        circle.style.backgroundColor = "#C4C4C4"
     } else {
         page3.style.display = "flex";
     }
 }
 
-function autoTab(current, to){
-    if (current.getAtributte && current.value.length==current.getAtributte("maxlength")){
-        to.focus()
-    }
-}
 
-function play(){
-    console.time();
-    console.timeLog();
+function play() {
     running = true;
-    clock();
     rotate(playPause);
     playPause.setAttribute("onclick", "pause()");
-    playPause.setAttribute("src", "icons/pause-circle.png")
+    playPause.setAttribute("src", "icons/pause-circle.png");
+
+    var update = setInterval(function(){
+        if (running){
+            timeCount -= 1;
+            display();
+            if ( timeCount <= 0) {
+                timeCount = 0;
+                clearInterval(update);
+                pause()
+            }
+        } else {
+            clearInterval(update);
+        }
+    }, 1000);
+
 }
 
 function pause() {
@@ -51,32 +66,23 @@ function pause() {
 }
 
 function restart(){
-    rt.innerHTML = timeTotal.innerHTML;
-    ct.innerHTML = 0;
     rotate(restartBtn);
     pause();
-    console.timeEnd();
-    console.clear();
+    clock();
 }
 
 // This makes the countdown work ones the time is given
 
 function clock(){
-    var update = setInterval(function(){
-        if (running){
-            rt.innerHTML -= 1;
-            ct.innerHTML -=  1;
-            console.timeLog();
-            if ( rt.innerHTML <= 0) {
-                console.timeEnd();
-                rt.innerHTML = 0;
-                clearInterval(update);
-                pause()
-            }
-        } else {
-            clearInterval(update);
-        }
-    }, 1000);
+    timeCount = document.getElementById("timeSelector").valueAsNumber;
+    timeCount /= (1000*60);
+    display();
+}
+
+function display(){
+    minutes = Math.floor(timeCount / 60);
+    seconds = timeCount - (minutes * 60);
+    remainingTime.innerHTML = minutes + " : " + seconds;
 }
 
 
